59f71aeff9c3b7ed8b61be0f31dc58115bc30364
Asma <asma.ha.almalki@gmail.com>
2025-04-20 00:56:15 +0300

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ st.markdown(
 )
 
 # الشعار والعنوان والتحذير
-st.image("download.png", width=180)
+st.image("download.png", width=120)
 st.markdown("## مساعد وزارة المالية")
 st.markdown("### إدارة الحوكمة والمخاطر والالتزام")
 st.markdown('<div class="warning">يرجى الالتزام بسياسات خصوصية البيانات وحماية البيانات الشخصية المعتمدة في وزارة المالية</div>', unsafe_allow_html=True)
